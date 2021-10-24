0833ba670466713e407e0a7dab7448b87ef9eac0
DDubicki <dubickidymitr@gmail.com>
2021-10-24 14:40:28 +0200

    Archer, Assassin, Swordsman classes added. Get full name implemented

--- src/main/java/com/codecool/gladiator/model/gladiators/Brutal.java
+++ src/main/java/com/codecool/gladiator/model/gladiators/Brutal.java
@@ -1,6 +1,7 @@
 package com.codecool.gladiator.model.gladiators;
 
 public class Brutal extends Gladiator {
+
     public Brutal(String name, int baseHp, int baseSp, int baseDex, int level) {
         super(name, baseHp, baseSp, baseDex, level);
     }
