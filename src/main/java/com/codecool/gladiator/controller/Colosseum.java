614cf553c19f57f9abde9c30ce1a2b89d6e8aed7
DDubicki <dubickidymitr@gmail.com>
2021-10-28 19:17:14 +0200

    Split gladiators into pairs - done

--- src/main/java/com/codecool/gladiator/controller/Colosseum.java
+++ src/main/java/com/codecool/gladiator/controller/Colosseum.java
@@ -53,11 +53,18 @@ public class Colosseum {
     }
 
     private List<Contestants> splitGladiatorsIntoPairs(List<Gladiator> gladiators) {
-        // Todo
-        return new LinkedList<>();
+        LinkedList<Contestants> gladiatorsPairs = new LinkedList<>();
+        for (int i = 0; i < gladiators.size(); i = i + 2) {
+            Gladiator gladiator1 = gladiators.get(i);
+            Gladiator gladiator2 = gladiators.get(i + 1);
+            Contestants contestants = new Contestants(gladiator1, gladiator2);
+            gladiatorsPairs.add(contestants);
+        }
+        return gladiatorsPairs;
     }
 
     private Gladiator getChampion(Tournament tournament) {
+
         // Todo - call simulateCombat as many times as needed
         return null;
     }
@@ -90,7 +97,7 @@ public class Colosseum {
 
     private void introduceGladiators(List<Gladiator> gladiators) {
         view.display(String.format("\nWe have selected Rome's %d finest warriors for today's Tournament!", gladiators.size()));
-        for (Gladiator gladiator: gladiators) {
+        for (Gladiator gladiator : gladiators) {
             view.display(String.format(" - %s", gladiator.getFullName()));
         }
         view.display("\n\"Ave Imperator, morituri te salutant!\"");
